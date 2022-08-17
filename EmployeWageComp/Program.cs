0027ed009b61a994ddaf7953af01885b878468c3
imohtesham <ibraahimm92@gmail.com>
2022-08-17 12:01:57 +0530

    add attendance

--- EmployeWageComp/Program.cs
+++ EmployeWageComp/Program.cs
@@ -10,7 +10,22 @@ namespace EmployeWageComp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("welcome to employe wage");
+            int employePresent = 5;
+
+            Random random = new Random();
+
+            int empCheck = random.Next(0, 6);
+
+
+            if (employePresent == empCheck)
+
+                Console.WriteLine("Employe is present");
+
+
+            else
+                Console.WriteLine("Employe is absent");
+
+            Console.ReadLine();
 
         }
     }
