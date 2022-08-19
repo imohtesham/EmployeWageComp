617d60ad9426d8718198d4b77cff7aed4455539f
imohtesham <ibraahimm92@gmail.com>
2022-08-19 16:27:52 +0530

    Monthly wage

--- EmployeWageComp/Program.cs
+++ EmployeWageComp/Program.cs
@@ -8,42 +8,41 @@ namespace EmployeWageComp
 {
     public class Program
     {
-       public const int PART_TIME = 1;
+        public const int PART_TIME = 1;
         public const int FULL_TIME = 2;
         public const int PER_HOUR_RATE = 20;
+        public const int NUM_OF_WORKING_DAY = 20;
 
         static void Main(string[] args)
         {
             int EMPHR = 0;
-            int EMPWGE = 0;
+            int empwge = 0;
+            int totalempwage = 0;
 
-            Random random = new Random();       
-            int empcheck = random.Next(0,3);
-
-            switch (empcheck)
+            for (int day = 0; day <NUM_OF_WORKING_DAY; day++)
             {
-                case PART_TIME:
-                    EMPHR = 4;
-                    break;
-                case FULL_TIME:
-                    EMPHR= 8;
-                    break;
-                default:    
-                    EMPHR = 0;
-                    break;
+                Random random = new Random();
+                int empcheck = random.Next(0, 3);
+
+                switch (empcheck)
+                {
+                    case PART_TIME:
+                        EMPHR = 4;
+                        break;
+                    case FULL_TIME:
+                        EMPHR = 8;
+                        break;
+                    default:
+                        EMPHR = 0;
+                        break;
+                }
+                empwge = EMPHR * PER_HOUR_RATE;
+                totalempwage += empwge;
+                Console.WriteLine("emp wage :" + empwge);
             }
-               
-
-            EMPWGE = EMPHR * PER_HOUR_RATE;
-            Console.WriteLine("emp wage :" + EMPWGE);
+            Console.WriteLine("totalempwage :" + totalempwage);
+            
         }
-        
-           
-            
-
-
-            
-
            
     }
 }
